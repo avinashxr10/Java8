3b9f1b32a00393ee343e9e8e1c26135e2a1208f8
Avinash Kumar <avinashxr10@gmail.com>
2022-02-20 22:52:09 +0530

    Stream Filter and Sorting

---
+++ src/com/java/java8/Stream/StreamColonOperator.java
@@ -0,0 +1,18 @@
+package com.java.java8.Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class StreamColonOperator {
+    public static void main(String[] args) {
+        List<String> alphaNumerics = Arrays.asList("A11","A44","a22","a49","G66");
+        List<String> strList = alphaNumerics.stream().map(String::toUpperCase).filter(e -> e.startsWith("A")).collect(Collectors.toList());
+        System.out.println(strList);
+
+        List<String> str = alphaNumerics.stream().map(String :: toUpperCase).filter(e -> e.startsWith("A")).collect(Collectors.toList());
+        System.out.println(str);
+    }
+}
