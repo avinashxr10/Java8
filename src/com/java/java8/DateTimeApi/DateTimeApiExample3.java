b911b83c8ed55845191eb2aa5c8c367b4d69ed09
Avinash Kumar <avinashxr10@gmail.com>
2022-02-24 19:39:51 +0530

    Date Time Between

---
+++ src/com/java/java8/DateTimeApi/DateTimeApiExample3.java
@@ -0,0 +1,20 @@
+package com.java.java8.DateTimeApi;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class DateTimeApiExample3 {
+    public static void main(String[] args) {
+        // Get the total experience of an employee in years
+        LocalDate todayDate = LocalDate.now();
+        LocalDate startDate = LocalDate.of(2012, 3, 16);
+
+        //Years of experience
+        Period period = Period.between(startDate, todayDate); // startDate is inclusive and todayDate is exclusive
+        System.out.println("Your years of experience is "+period.getYears());
+
+        System.out.printf("Total experience is %d years, %d months, %d days ",period.getYears(), period.getMonths(), period.getDays());
+
+        //you have exam at a date, you want to know how much time you have
+    }
+}
---
+++ src/com/java/java8/DateTimeApi/DateTimeApiExample4.java
@@ -0,0 +1,4 @@
+package com.java.java8.DateTimeApi;
+
+public class DateTimeApiExample4 {
+}
